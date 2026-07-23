182110bcf8e5934267cb57acf359ee6aa0265067
Nikolay Dimitrov <n.dimitrov@draftkings.com>
2026-07-23 18:08:02 +0300

    feat(02): first real API test - GET /ping with status assertion
    Sub-session B of SDP-37391. RestSharp NuGet added.
    PingTests uses SetUp/TearDown pattern, async/await, and
    Assert.That with HttpStatusCode.Created enum.

---
+++ 02-restsharp-api/RestfulBookerTests/PingTests.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using RestSharp;
+
+namespace RestfulBookerTests;
+
+[TestFixture]
+public class PingTests
+{
+    private RestClient _client;
+
+    [SetUp]
+    public void Setup()
+    {
+        _client = new RestClient("https://restful-booker.herokuapp.com");
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+        _client.Dispose();
+    }
+
+    [Test]
+    public async Task Ping_ReturnsCreated()
+    {
+        // Arrange
+        var request = new RestRequest("/ping", Method.Get);
+
+        // Act
+        var response = await _client.ExecuteAsync(request);
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+    }
+}
